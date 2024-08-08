9a0c155b6287f091ac804a9495744de9702b7be5
azimovgleb <azimov.gleb96@gmail.com>
2024-08-08 18:02:58 +0300

    fix lib commit

--- src/main/java/org/example/dataanalysermicroservice/config/BeanConfig.java
+++ src/main/java/org/example/dataanalysermicroservice/config/BeanConfig.java
@@ -6,8 +6,6 @@ import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
-
 @Configuration
 public class BeanConfig {
 
