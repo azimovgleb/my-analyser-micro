98e50ec3cb1d9e8213f41ed344cd30dd6f765696
azimovgleb <azimov.gleb96@gmail.com>
2024-08-08 18:01:58 +0300

    init commit

---
+++ src/main/java/org/example/dataanalysermicroservice/service/KafkaDataServiceImpl.java
@@ -0,0 +1,22 @@
+package org.example.dataanalysermicroservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.dataanalysermicroservice.model.Data;
+import org.example.dataanalysermicroservice.repository.DataRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaDataServiceImpl implements KafkaDataService{
+
+    private final DataRepository dataRepository;
+
+    @Override
+    public void handle(Data data) {
+        log.info("Data object {} was saved", data);
+        dataRepository.save(data);
+    }
+
+}
---
+++ src/main/resources/liquibase/changesets/001_create_tables.sql
@@ -0,0 +1,9 @@
+create table IF NOT EXISTS data
+(
+    id          bigserial primary key,
+    sensor_id    bigint    not null,
+    timestamp   timestamp not null,
+    measurement float     not null,
+    type        varchar   not null
+
+);
